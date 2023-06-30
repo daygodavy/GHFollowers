4c7a9c1ae8c27e72fd25c673383d950a428ad10f
Davy <dchuon@ucdavis.edu>
2023-06-30 12:32:57 -0700

    Setup scenedelegate with storyboard removed and imported assets

---
+++ GHFollowers/GHFollowers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Davy Chuon on 6/30/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemPink
+    }
+
+
+}
+
