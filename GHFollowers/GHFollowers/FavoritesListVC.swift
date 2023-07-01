c8c5b6eb64abd96fcf580c758e0a517fe56df648
Davy <dchuon@ucdavis.edu>
2023-07-01 12:17:49 -0700

    created GFButton and GFTextfield classes

--- GHFollowers/GHFollowers/FavoritesListVC.swift
+++ GHFollowers/GHFollowers/FavoritesListVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  FavoritesListVC.swift
 //  GHFollowers
 //
 //  Created by Davy Chuon on 6/30/23.
@@ -7,14 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FavoritesListVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.backgroundColor = .systemPink
+        view.backgroundColor = .systemBlue
     }
-
-
 }
-
---
+++ GHFollowers/GHFollowers/SearchVC.swift
@@ -0,0 +1,16 @@
+//
+//  SearchVC.swift
+//  GHFollowers
+//
+//  Created by Davy Chuon on 6/30/23.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+}
