5dfe79504ccb520f1ff20600e3fcd25f050a2d44
Davy <dchuon@ucdavis.edu>
2023-07-01 15:03:22 -0700

    created Follower model and User model

---
+++ GHFollowers/GHFollowers/Model/Follower.swift
@@ -0,0 +1,14 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Davy Chuon on 7/1/23.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+    
+}
