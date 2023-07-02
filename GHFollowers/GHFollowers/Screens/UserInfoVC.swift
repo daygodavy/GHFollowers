20b9c86b0506a9e40ceec92635da639d2986c912
Davy <dchuon@ucdavis.edu>
2023-07-01 22:11:30 -0700

    created UserInfoVC and is now presented when follower is tapped from FollowerListVC

---
+++ GHFollowers/GHFollowers/Screens/UserInfoVC.swift
@@ -0,0 +1,26 @@
+//
+//  UserInfoVC.swift
+//  GHFollowers
+//
+//  Created by Davy Chuon on 7/1/23.
+//
+
+import UIKit
+
+class UserInfoVC: UIViewController {
+    
+    var username: String!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissVC))
+        navigationItem.rightBarButtonItem = doneButton
+    }
+    
+    
+    @objc func dismissVC() {
+        dismiss(animated: true)
+    }
+
+}
